52d93837612484d5ae12328cf13ee38dbb362a36
rsgitt <rsmymail@proton.me>
2023-10-29 16:21:53 +0100

    ostatnia zmiana

---
+++ 29.10.2023/zad7.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+// do zrobienia
+int main()
+{
+    int day, month, year;
+    printf("Podaj dzien, miesiac i rok a ja sprawdze czy podane liczby\n");
+    scanf("%d%d%d", &day, &month, &year);
+    if ((year >= 1 && year <= 2023) )
+
+}
