ab0af1b021e10138722ad2e9d56fc0fc9c1643d2
Robert Siewruk <robert.swrk@gmail.com>
2023-11-02 12:57:11 +0100

    new file

--- 29.10.2023/zad7.c
+++ 29.10.2023/zad7.c
@@ -1,10 +1,19 @@
+//Napisz program który pobierze od użytkownika trzy liczby (dzień, miesiąc, rok) i sprawdzi czy tworzą poprawną datę.
 #include<stdio.h>
-// do zrobienia
+
 int main()
 {
     int day, month, year;
-    printf("Podaj dzien, miesiac i rok a ja sprawdze czy podane liczby\n");
+    int max_year = 2023;
+    printf("Podaj po kolei dzien, miesiac i rok a ja sprawdze czy podane liczby: ");
     scanf("%d%d%d", &day, &month, &year);
-    if ((year >= 1 && year <= 2023) )
 
+    if ((day >=1 && day <=31) && (month >=1 && month <= 12) && (year >= 1 && year <= max_year))
+    {
+        printf("Data %d.%d.%d jest poprawna", day, month, year);
+    }
+    else
+    {
+        printf("Data %d.%d.%d nie jest poprawna", day, month, year);
+    }
 }
