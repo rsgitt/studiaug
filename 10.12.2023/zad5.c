6f471c1243b784dd360062e86705df92ea0f34d2
rsgitt <rsmymail@proton.me>
2023-12-12 20:49:16 +0100

    code refactorization

--- 10.12.2023/zad5.c
+++ 10.12.2023/zad5.c
@@ -10,47 +10,35 @@ void changePlaces(int * num1, int * num2)
 }
 void sortHighToLow(int * num1, int * num2, int * num3)
 {
-    int waiting;
-
     if ((*num1 > *num2) && (*num2 > *num3) && (*num1 > *num3)) //np 5, 4, 3
     {
-        waiting = *num1;
-        *num1 = *num3;
-        *num3 = waiting;
+        changePlaces(num1, num3);
+    }
+    if ((*num1 < *num2) && (*num2 > *num3) && (*num1 < *num3)) //np. 3, 5, 4
+    {
+        changePlaces(num2, num3);
+    }
+    if ((*num1 > *num2) && (*num2 < *num3) && (*num1 < *num3)) //np. 4, 3, 5
+    {
+        changePlaces(num1, num2);
+    }
+    if ((*num1 > *num2) && (*num2 < *num3) && (*num1 > *num3)) //np. 5, 3, 4
+    {
+        changePlaces(num1, num3);
+        changePlaces(num1, num2);
     }
     if ((*num1 < *num2) && (*num2 > *num3) && (*num1 > *num3)) //np 4, 5, 3
     {
         changePlaces(num2, num3);
-        waiting = *num2;
-        *num2 = *num1;
-        *num1 = waiting;
+        changePlaces(num1, num2);
     }
-    if ((*num1 < *num2) && (*num2 > *num3) && (*num1 < *num3)) //np. 3, 5, 4
-    {
-        waiting = *num2;
-        *num2 = *num3;
-        *num3 = waiting;
-    }
-    if ((*num1 > *num2) && (*num2 < *num3) && (*num1 < *num3)) //np. 4, 3, 5
-    {
-        waiting = *num2;
-        *num2 = *num1;
-        *num1 = waiting;
-    }
-    if ((*num1 > *num2) && (*num2 < *num3) && (*num1 < *num3)) //np. 5, 3, 4
-    {
-        changePlaces(num2, num3);
-        waiting = *num1;
-        *num1 = *num3;
-        *num3 = waiting;
-    }
-
 }
 int main()
 {
     int num1 = 5;
-    int num2 = 4;
-    int num3 = 6;
+    int num2 = 3;
+    int num3 = 4;
+
     int * wsk1 = &num1;
     int * wsk2 = &num2;
     int * wsk3 = &num3;
