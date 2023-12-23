0556d007cd1e4a30742abd0164e003b265d2bcee
rsgitt <rsmymail@proton.me>
2023-12-23 18:42:23 +0100

    done zad15.c

--- 19.11.2023/zad15.c
+++ 19.11.2023/zad15.c
@@ -1,6 +1,46 @@
 //Napisz funkcję, która przyjmuje jako argument tablicę znaków i wypisze na ekran prostokątną ramkę wypełnioną znakami z tej tablicy.
 #include<stdio.h>
-int mian()
+void addElementsTable(int * numTab, int size)
 {
-    
+    for (int i=0; i<size; i++)
+    {
+        printf("Podaj element do tablicy\n");
+        scanf("%d", &numTab[i]);
+    }
+    printf("\n");
+}
+
+void startEndTable(int size)
+{
+    printf("|");
+    for (int i=0; i<size; i++)
+    {
+        printf("---");
+    }
+    printf("|\n");
+}
+
+void printRectangleTable(int * numTab, int size)
+{
+    startEndTable(size);
+    printf("|");
+
+    for (int i=0; i<size; i++)
+    {
+        printf(" %d ", numTab[i]);
+    }
+
+    printf("|\n");
+    startEndTable(size);
+}
+
+int main()
+{
+    int size;
+    printf("Podaj rozmiar tablicy\n");
+    scanf("%d", &size);
+    int numTab[size];
+
+    addElementsTable(numTab, size);
+    printRectangleTable(numTab, size);
 }
