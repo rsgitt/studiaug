51141b5959639a08f2f9def4bead19301c4f8320
rsgitt <rsmymail@proton.me>
2023-12-30 23:06:26 +0100

    done zad11.c

--- 29.10.2023/zad11.c
+++ 29.10.2023/zad11.c
@@ -2,32 +2,63 @@
 //todo
 #include<stdio.h>
 
-int biggest_num(int a, int b, int c)
+int positiveNums(int side1, int side2, int side3)
 {
-    printf("Podaj trzy liczby: ");
-    scanf("%d %d %d", &a, &b, &c);
-
-    if (a >= b && a >= c) {
-        return a, b, c;
-    } else if (b >= a && b >= c) {
-        return b, a, c;
-    } else {
-        return c, b, a;
+    if ((side1 > 0 )&& (side2 > 0) && (side3 > 0))
+    {
+        return 1;
     }
-}
+    return 0;
 }
 
-int check_triangle(int biggest_side, int side2, int side3);
+int power(int num)
+{
+    num *= num;
+    return num;
+}
+
+int checkRightTriangle(int side1, int side2, int side3)
 {
 
+    if (positiveNums(side1, side2, side3))
+    {
+        int powerSide1 = power(side1);
+        int powerSide2 = power(side2);
+        int powerSide3 = power(side3);
+
+        if ((powerSide1 + powerSide2) == powerSide3)
+        {
+            return side3;
+        }
+
+        if ((powerSide1 + powerSide3) == powerSide2)
+        {
+            return side2;
+        }
+
+        if ((powerSide2 + powerSide3) == powerSide1)
+        {
+            return side1;
+        }
+
+
+    }
+    return 0;
 }
 
 int main()
 {
     int side1, side2, side3;
-    int 
+    int hypotenuse;
     printf("Podaj jakiekolwiek 3 dlugosci bokow a ja sprawdze czy mozna z nich utworzyc trojkat prostokatny: \n");
-    scanf("%d%d%d", side1, side2, side3);
-    check_triangle(side1, side2, side3)
-
+    scanf("%d%d%d", &side1, &side2, &side3);
+    hypotenuse = checkRightTriangle(side1, side2, side3);
+    if (hypotenuse)
+    {
+        printf("Mozna stworzyc trojkat prostokatny z pdanych bokow, jego przeciwprostokatna to %d", hypotenuse);
+    }
+    else
+    {
+        printf("Nie mozna stworzyc z podanych bokow trojkata prostokatnego");
+    }
 }
